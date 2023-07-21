a377c7078b2918cc477623f04e7e06439480630b
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-07-21 11:50:46 -0400

    Add Api connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-orange-400">Hello world!</h1>
-    </>
-  );
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://www.plugco.in/public/take_home_sample_feed");
+        const jsonData = await response.json();
+        setData(jsonData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
+
+  return <div>{isLoading ? <h1>Loading...</h1> : <Home data={data} />}</div>;
 }
 
 export default App;
